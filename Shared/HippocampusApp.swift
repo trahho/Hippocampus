9b534909fe228a2d94433041669bb16c75f0973e
trahho <Guido.Kuehn@trahho.de>
2022-07-29 23:19:43 +0200

    Initial Commit

---
+++ Shared/HippocampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HippocampusApp.swift
+//  Shared
+//
+//  Created by Guido Kühn on 29.07.22.
+//
+
+import SwiftUI
+
+@main
+struct HippocampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
