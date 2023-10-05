7f8a16674513fb7926717adbda2c1efc18d0620b
trahho <Guido.Kuehn@trahho.de>
2023-10-05 18:22:12 +0200

    Für neue Settings

---
+++ Display Layer/Design/Design.MiniMindMapView.swift
@@ -0,0 +1,18 @@
+//
+//  Design.MiniMindMapView.swift
+//  Hippocampus
+//
+//  Created by Guido Kühn on 05.10.23.
+//
+
+import SwiftUI
+
+struct Design_MiniMindMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Design_MiniMindMapView()
+}
